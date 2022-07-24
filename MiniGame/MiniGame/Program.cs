27713b385614a2776dad3f96cc040fe086ff55d0
Andriana <khariv.a.r@gmail.com>
2022-07-24 18:11:48 +0300

    added random

--- MiniGame/MiniGame/Program.cs
+++ MiniGame/MiniGame/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
-
-namespace MiniGame
+﻿namespace MiniGame
 {
+    public class SuperPowerGenerator
+    {
+        private int SuperPower;
+
+        public int GetSuperPower()
+        {
+            do
+            {
+                int TimeMilli = int.Parse(DateTime.Now.ToString("ffff"));
+                int TimeSec = int.Parse(DateTime.Now.ToString("ss"));
+                int TimeMin = int.Parse(DateTime.Now.ToString("mm"));
+
+                SuperPower = TimeMilli * TimeMilli % TimeSec;
+
+            } while (SuperPower >= 10 || SuperPower <= 5);
+
+            return SuperPower;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SuperPowerGenerator superPowerGenerator = new SuperPowerGenerator();
+            int superPower = superPowerGenerator.GetSuperPower();
+            Console.WriteLine(superPower);
+
         }
     }
 }
